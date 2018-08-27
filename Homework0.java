b3474ad62a7518c7a532749d805151c6f876efc4
marcus-rose <rosemar@berkeleyprep.org>
2018-08-27 08:04:04 -0400

    First commit

--- Homework0.java
+++ Homework0.java
@@ -1,5 +1,5 @@
 /*
- * First and Last Names
+ * Marcus Rose
  */
 
 public class Homework0 {
@@ -10,6 +10,9 @@ public class Homework0 {
 	 */
 	public static void main(String[] args) {
 
-	}
+		System.out.println("Hello, World!");
+
+
+}
 
 }
